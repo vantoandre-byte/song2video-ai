cb7b445bbf5f0e6fc261a82b92e7d9d09d7ffd66
vantoandre-byte <vanto.andre@gmail.com>
2026-07-26 05:47:17 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import '
-
 import 'core/theme/app_theme.dart';
 import 'screens/splash/splash_screen.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  
-
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Hive.initFlutter();
